4e818b64bd2610e26836727063ae154c6f7ae3d9
yonahcodes <yonahcodes@gmail.com>
2025-01-05 16:25:31 -0500

    makes10.py

---
+++ warmup_1/makes10.py
@@ -0,0 +1,18 @@
+"""
+Warmup-1 > males10
+------------------
+
+Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → True
+makes10(9, 9) → False
+makes10(1, 9) → True
+
+Date: 05 01 2025
+"""
+
+def makes10(a, b):
+    if (a == 10 or b == 10) or a + b == 10:
+        return True
+    else:
+        return False
