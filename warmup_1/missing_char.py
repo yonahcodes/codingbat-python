8fb6f4c48204b37533349f9cb957790a7195f0d7
yonahcodes <yonahcodes@gmail.com>
2025-01-14 23:14:25 -0500

    missing_char.py

---
+++ warmup_1/missing_char.py
@@ -0,0 +1,23 @@
+"""
+Warmup-1 > missing_char
+-----------------------
+
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. 
+The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+missing_char('kitten', 1) → 'ktten'
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+
+Date: 14 01 2025
+"""
+
+def missing_char(str, n):
+    if str:
+        return str[:n] + str[n + 1:]
+
+
+def missing_char(str, n):
+  front = str[:n]   # up to but not including n
+  back = str[n+1:]  # n+1 through end of string
+  return front + back
