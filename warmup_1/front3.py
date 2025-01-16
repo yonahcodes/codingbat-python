10acdd5ce578f953e849520a724f6b498fc7a1a2
yonahcodes <yonahcodes@gmail.com>
2025-01-15 21:40:11 -0500

    front3

--- warmup_1/front3.py
+++ warmup_1/front3.py
@@ -16,9 +16,19 @@ Date: 15 01 2025
 
 
 def front3(str):
-    if len(str) >= 3:
-        return str[:3] + str[:3] + str[:3]
-    else:
-        return str
+  front = str[:3]
+  if len(str) < 3:
+      front = str
+  return front + front + front
 
-print(front3("abc"))
+
+# def front3(str):
+#   # Figure the end of the front
+#   front_end = 3
+#   if len(str) < front_end:
+#     front_end = len(str)
+#   front = str[:front_end]
+#   return front + front + front
+
+# Could omit the if logic, and write simply front = str[:3]
+# since the slice is silent about out-of-bounds conditions.
