0618affb7569206ae3cd29a02672cf561d91b42a
yonahcodes <yonahcodes@gmail.com>
2025-01-17 14:21:28 -0500

    front_times.py

--- warmup_2/front_times.py
+++ warmup_2/front_times.py
@@ -2,7 +2,8 @@
 Warmup 2 -> front_times
 ------------------------
 
-Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, 
+or whatever is there if the string is less than length 3. Return n copies of the front;
 
 front_times('Chocolate', 2) → 'ChoCho'
 front_times('Chocolate', 3) → 'ChoChoCho'
@@ -12,4 +13,17 @@ Date: 17 01 2025
 """
 
 def front_times(str, n):
-    
+    front = str[:3]
+    return front * n
+
+
+# def front_times(str, n):
+#   front_len = 3
+#   if front_len > len(str):
+#     front_len = len(str)
+#   front = str[:front_len]
+  
+#   result = ""
+#   for i in range(n):
+#     result = result + front
+#   return result
