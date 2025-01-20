6f6a8b6ea62f12cede1af94444444dcaf499e2bd
yonahcodes <yonahcodes@gmail.com>
2025-01-20 18:12:10 -0500

    string_splosion.py

--- warmup_2/string_bits.py
+++ warmup_2/string_bits.py
@@ -1,6 +1,6 @@
 """
 Warmup 2 -> string_bits
-------------------------
+-----------------------
 
 Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
 
---
+++ warmup_2/string_splosion.py
@@ -0,0 +1,15 @@
+"""
+Warmup 2 -> string_splosion
+---------------------------
+
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+string_splosion('Code') → 'CCoCodCode'
+string_splosion('abc') → 'aababc'
+string_splosion('ab') → 'aab'
+
+Date: 20 01 2025
+"""
+
+def string_splosion(str):
+    
